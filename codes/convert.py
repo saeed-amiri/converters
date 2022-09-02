be247e02d53032f0b08639e5bac9d4c4722a0426
Saeed Amiri <samiri@uni-bremen.de>
2022-09-02 10:23:19 +0200

    PEP8

--- codes/convert.py
+++ codes/convert.py
@@ -1,5 +1,7 @@
 import sys
 import json_to_lmp as jlmp
+import write_lmp as lmpwrt
+
 
 class Doc:
     """convert files in other format into LAMMPS data file
@@ -9,5 +11,9 @@ class Doc:
         a data file in LAMMPS
     """
 
+
 fname: str = sys.argv[1]  # File name to convert
 data = jlmp.ConvertJson(fname)
+output_fname = 'test.data'
+wrt = lmpwrt.WriteLmp(data, output_fname)
+wrt.write_lmp()
