b37db210fe6eee48b6f19f60e2ba1af086b7d20a
Saeed Amiri <samiri@uni-bremen.de>
2022-09-15 11:16:27 +0200

    add HATATM

--- codes/pdb_to_df.py
+++ codes/pdb_to_df.py
@@ -1,5 +1,6 @@
-import pandas as pd
+import sys
 import typing
+import pandas as pd
 
 
 class Pdb:
@@ -13,7 +14,15 @@ class Pdb:
     ATOM 	atomic coordinate record containing the X,Y,Z
         orthogonal Å coordinates for atoms in standard residues]
         (amino acids and nucleic acids).
-    Protein Data Bank Format:
+
+    HATATM 	atomic coordinate record containing the X,Y,Z orthogonal Å
+    coordinates for atoms in nonstandard residues. Nonstandard residues
+    include inhibitors, cofactors, ions, and solvent. The only functional
+    difference from ATOM records is that HETATM residues are by default
+    not connected to other residues. Note that water residues should be
+    in HETATM records.
+
+    Protein Data Bank Format for ATOM:
       Coordinate Section
         Record Type	Columns	Data 	Justification	Data Type
         ATOM 	1-4	“ATOM”	                    	character
@@ -32,6 +41,13 @@ class Pdb:
         73-76	Segment identifier¶	        left	character
         77-78	Element symbol              right	character
         79-80	Charge		                        character
+
+    Protein Data Bank Format for HATATM:
+      Coordinate Section
+        Record Type	Columns	Data 	Justification	Data Type
+        1-6	“HETATM”		character
+        7-80	same as ATOM records
+
     #Chimera allows (nonstandard) use of columns 6-11 for the integer
         atom serial number in ATOM records, and in TER records, only the
         “TER” is required.
@@ -51,35 +67,63 @@ class Pdb:
     Format (A6,I5,1X,A4,A1,A3,1X,A1,I4,A1,3X,3F8.3,2F6.2,10X,A2,A2)
     """
 
-    def __init__(self) -> None:
-        print(f"Reading '{PDBFILE}' ...")
+    def __init__(self,
+                 fname: str  # PDB file name
+                 ) -> None:
+        print(f"Reading '{fname}' ...")
+        self.get_data(fname)
 
-    def get_data(self) -> None:
+    def get_data(self,
+                 fname: str  # PDB file name
+                 ) -> None:
         """Read and get the atomic strcuture in lammps`"""
         # Read PDB file and return a list of list
-        data = self.read_pdb()
+        data = self.read_pdb(fname)
         # Convert the list to DataFrame
         df = self.mk_df(data)
         # Check the residue number order
         self.atoms_df = self.check_residue_number(df)
 
-    def read_pdb(self) -> list:
+    def read_pdb(self,
+                 fname: str  # PDB file name
+                 ) -> list:
         """Reading line by line of the pdb file"""
         data_list: list[typing.Any] = []
-        with open(PDBFILE, 'r') as f:
+        with open(fname, 'r') as f:
             while True:
                 line = f.readline()
                 if line.strip().startswith("ATOM"):
-                    data_list.append(self.__process_line(line))
+                    data_list.append(self.__process_ATOM(line))
+                if line.strip().startswith("HETATM"):
+                    data_list.append(self.__process_HETATM(line))
                 if not line:
                     break
         return data_list
 
-    def __process_line(self, line: str) -> list:
-        """Process the line on based on the PDB file"""
+    def __process_ATOM(self, line: str) -> list:
+        """Process the line on based on the PDB file, started with ATOM"""
         # first check the length of the line MUST be equal to 79
         if len(line) != 79:
             exit(f"ERROR! wrong line length: {len(line)} != 79")
+        atom_id: int = int(line[6:11].strip())
+        atom_name: str = line[13:16].strip()
+        residue_name: str = line[17:20].strip()
+        residue_number: str = line[22:27].strip()
+        x: float = float(line[30:39].strip())
+        y: float = float(line[39:47].strip())
+        z: float = float(line[47:55].strip())
+        atom_symbol: str = line[76:78].strip()
+        return [atom_id,
+                atom_name,
+                residue_name,
+                residue_number,
+                x,
+                y,
+                z,
+                atom_symbol]
+
+    def __process_HETATM(self, line: str) -> list:
+        """Process the line on based on the PDB file, started with HATATM"""
         atom_id: int = int(line[6:11].strip())
         atom_name: str = line[13:16].strip()
         residue_name: str = line[17:20].strip()
@@ -138,3 +182,8 @@ class Pdb:
         del df_reduced
         del residue_dict
         return df
+
+
+if __name__ == '__main__':
+    pdb = Pdb(sys.argv[1])
+    print(pdb.atoms_df)
