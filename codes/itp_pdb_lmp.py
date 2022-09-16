09cca1ee915611491313a20f228d8dc1cc1f69e5
Saeed Amiri <samiri@uni-bremen.de>
2022-09-16 11:53:30 +0200

    make the LAMMPS df2

--- codes/itp_pdb_lmp.py
+++ codes/itp_pdb_lmp.py
@@ -1,4 +1,25 @@
 import typing
 import pandas as pd
-import itp_to_df as itp
-import pdb_to_df as pdb
+from itp_to_df import Itp
+from pdb_to_df import Pdb
+
+class Doc:
+    """call the classes to read itp and pdb files and return data
+    in format that can be write into LAMMPS files"""
+
+
+class ItpPdb(Pdb,  # class which give dataframe for the pdb file
+             Itp  # class which give dataframe for the itp file
+             ):
+    """call all the classes and update the Atoms_df and all other dfs
+    so can be writeed by write_lmp.py
+    """
+    def __init__(self,
+                 fname: str  # itp file name, for pdb it shoud write
+                 ) -> None:
+        Pdb.__init__(self, f'{fname}.pdb')
+        Itp.__init__(self, f'{fname}.itp')
+        print(self.atoms_extra)
+
+if __name__ == '__main__':
+    data = ItpPdb('UND')
