82548f3c67c4cfb5377a8ac539187ecc9175ee33
Saeed Amiri <samiri@uni-bremen.de>
2022-09-16 11:50:46 +0200

    make data from itp and pdb

--- codes/convert.py
+++ codes/convert.py
@@ -1,19 +1,26 @@
 import sys
 import json_to_lmp as jlmp
 import write_lmp as lmpwrt
-
+import itp_pdb_lmp as ipl
 
 class Doc:
     """convert files in other format into LAMMPS data file
     Input:
         a data file in JSON
+        or
+        a data file in itp (it needs two file with same name with .pdb)
     Output:
         a data file in LAMMPS
     """
 
 
 fname: str = sys.argv[1]  # File name to convert
-data = jlmp.ConvertJson(fname)
-output_fname = 'test.data'
-wrt = lmpwrt.WriteLmp(data, output_fname)
-wrt.write_lmp()
+f_extansion: str  # Extension of the file to call proper function
+f_extansion = fname.split('.')[1]
+if f_extansion == 'json':
+    data = jlmp.ConvertJson(fname)
+    output_fname = 'test.data'
+    wrt = lmpwrt.WriteLmp(data, output_fname)
+    wrt.write_lmp()
+if f_extansion == 'itp':
+    pass
