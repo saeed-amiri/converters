878f606e83ae60c994dafd7c6031bf393136b907
Saeed Amiri <samiri@uni-bremen.de>
2022-09-21 12:19:40 +0200

    make uniqe type of angles

--- codes/clean_data.py
+++ codes/clean_data.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+import re
+import typing
 import pandas as pd
 import sys
 import read_lmp_data as rdlmp
@@ -94,6 +96,7 @@ class CleanData:
         types: list[int]  # Angles type from names
         names = self.angles_name()
         types = get_type(names)
+        self.angles_type(names)
         columns: list[str]  # DataFrame columns for angles in LAMMPS
         columns = ['typ', 'ai', 'aj', 'ak', 'cmt', 'name']
         df: pd.DataFrame  # Angles df to write out
@@ -133,6 +136,59 @@ class CleanData:
                   ]
         angles = [f'{i}_{j}_{k}' for i, j, k in zip(ai_name, aj_name, ak_name)]
         return angles
+
+    def angles_type(self,
+                    names: list[str]  # Names of the bonds
+                    ) -> list[int]:  # Type of the angles
+        """make a correct type for angles
+        Angle ABC is same as CBA
+        Fixing this is complicated since it entirely depends on the
+        names of the type of atoms in the Mass section.
+        """
+        # Remove digits from the name of the atoms:
+        tmp_name: list[list[str]]  # name of the bonds without digits
+        tmp_name = [re.sub("\d+", "", item) for item in names]
+        tmp_lst: list[list[str]] = [item.split('_') for item in tmp_name]
+        # uniqe names after removing digits:
+        name_set: set[str]
+        name_set = self.seen_set(tmp_name)  # Remove the duplicates
+        name_set = [item.split('_') for item in name_set]
+        name_set = [item for sublist in name_set for item in sublist]
+        type_set = self.seen_set(name_set)  # Final set of uniqes atoms
+        # Set an index to each atom:
+        type_dict: dict[str, int]  # dict for name and type
+        type_dict = {item: v+1 for v, item in enumerate(type_set)}
+        # new type for all sets of angles
+        name_lst: list[list[str]]  # type of each set of angles
+        name_lst = [[str(type_dict[i]) for i in item] for item in tmp_lst]
+        # Make one list of all the angles with thier atom types
+        name_str: list[str]  # make a str list of int of types
+        name_str = ['_'.join(item) for item in name_lst]
+        # Get the uniqe type of all angles
+        seen: set[str] = set()
+        name_str = [tuple(item) for item in name_str]
+        # Make a list of each set of angles
+        t: list[str] = [
+            x for x in name_str if frozenset(x) not in seen and
+            not seen.add(frozenset(x))
+            ]
+        t = [list(item) for item in t]
+        t = [''.join(item) for item in t]
+        # Give a name to each uniqe set of angle
+        angle_type: dict[str, int]
+        angle_type = {item: v+1 for v, item in enumerate(t)}
+        print(angle_type)
+
+
+    def seen_set(self,
+                 lst: list[typing.Any]  # to drop duplicate with keping order
+                 ) -> list[typing.Any]:
+        """remove duplicated item with keeping order of them in the
+        main list"""
+        seen: set[str] = set()
+        seen_add = seen.add
+        return [x for x in lst if not (x in seen or seen_add(x))]
+
 
     def get_dihedrals(self) -> pd.DataFrame:  # Dihedrals DataFrame for writing
         """correct the name and type of the dihedrals"""
