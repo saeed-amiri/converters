d7172aa53e48567f1878d8e16a82f09f00cca356
Saeed Amiri <samiri@uni-bremen.de>
2022-09-26 14:14:09 +0200

    set atoms LJ interactions

--- codes/read_param.py
+++ codes/read_param.py
@@ -20,11 +20,51 @@ class ReadParam:
     def __init__(self,
                  fname: str = 'param'  # Name of the parameter file
                  ) -> None:
-        self.get_params(fname)
+        self.set_fields(fname)
+
+    def set_fields(self,
+                   fname: str  # Name of the input file
+                   ) -> None:  # Set attributes to self
+        """set force filed for each section to self"""
+        atoms: list[str]  # Atoms informations
+        bonds: list[str]  # Bonds informations
+        angles: list[str]  # Angles informations
+        dihedrals: list[str]  # Dihedrals informations
+        atoms, bonds, angles, dihedrals = self.get_params(fname)
+        if atoms:
+            self.atoms: pd.DataFrame = self.get_atoms(atoms)  # LJ information
+
+    def get_atoms(self,
+                  atoms: list[str]  # atoms line in param file
+                  ) -> pd.DataFrame:  # Atoms LJ information
+        """convert info to dataframe"""
+        columns: list[str]  # Columns for the df
+        columns =['atom_name', 'mass', 'sigma', 'epsilom', 'charge']
+        df = pd.DataFrame(columns=columns)
+        atom_name: list[str] = []  # To save from each line
+        mass: list[str] = []  # To save from each line
+        sigma: list[str] = []  # To save from each line
+        epsilom: list[str] = []  # To save from each line
+        charge: list[str] = []  # To save from each line
+        for item in atoms:
+            l_line: list[str]  # breaking the line
+            l_line = item.strip().split(' ')
+            atom_name.append(l_line[1])
+            mass.append(l_line[2])
+            sigma.append(l_line[3])
+            epsilom.append(l_line[4])
+            charge.append(l_line[5])
+        df['atom_name'] = atom_name
+        df['mass'] = mass
+        df['sigma'] = sigma
+        df['epsilom'] = epsilom
+        df['charge'] = charge
+        return df
 
     def get_params(self,
                    fname: str  # Name of the input file
-                   ) -> None:  # set attributs to self
+                   ) -> tuple[list[str], list[str], list[str], list[str]]:
+                   # return lines about atoms, bonds, angles, dihedrals
         """read and call all the methods"""
         atoms: list[str] = []  # Atoms informations
         bonds: list[str] = []  # Bonds informations
@@ -50,7 +90,8 @@ class ReadParam:
                     pass
                 if not line:
                     break
-
+        return atoms, bonds, angles, dihedrals
+        
 
 if __name__ == '__main__':
     fname: str = sys.argv[1]  # Input file
