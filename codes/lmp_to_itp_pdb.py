213fec4fb86968a21fb93af2e303e2319479d849
Saeed Amiri <samiri@uni-bremen.de>
2022-12-29 10:46:08 +0100

    read lammps data file

--- codes/lmp_to_itp_pdb.py
+++ codes/lmp_to_itp_pdb.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import read_lmp_data as relmp
 
@@ -9,3 +10,21 @@ class Doc:
         Name of the atoms in Masses section should be the ones in the
         force filed file in GROMACS
     """
+
+
+class ReadLmp:
+    """read lammps from read_lmp_data.py"""
+    def __init__(self,
+                 fname: str  # Input file name
+                 ) -> None:
+        self.get_lmp(fname)
+
+    def get_lmp(self,
+                fname: str  # Input file name
+                ) -> None:
+        """get the data"""
+        self.lmp_data = relmp.ReadData(fname)
+
+
+if __name__ == '__main__':
+    lmp: relmp.ReadData = ReadLmp(sys.argv[1])  # All data in input file
