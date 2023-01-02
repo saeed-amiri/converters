11b34e3a0f97941ec18b8cb6ff426c4ee3dfe489
Saeed Amiri <samiri@uni-bremen.de>
2023-01-02 10:11:29 +0100

    make itp file

--- codes/lmp_to_itp.py
+++ codes/lmp_to_itp.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import read_lmp_data as relmp
+import lmp_to_pdb as lmpdb
 from colors_text import TextColor as bcolors
 
 
@@ -34,41 +35,59 @@ class ItpStyleInfos:
 class Itp:
     """get data from main"""
     def __init__(self,
-                 lmp: relmp.ReadData  # LAMMPS data file
+                 lmp: relmp.ReadData,  # LAMMPS data file
+                 pdb_df: pd.DataFrame  # Final df for pdb file
                  ) -> None:
-        self.mk_itp(lmp)
+        self.mk_itp(lmp, pdb_df)
 
     def mk_itp(self,
-               lmp: relmp.ReadData  # LAMMPS data file
+               lmp: relmp.ReadData,  # LAMMPS data file
+               pdb_df: pd.DataFrame  # Final df for pdb file
                ) -> None:
         """call functions"""
-        self.mk_atoms(lmp)
+        self.mk_atoms(lmp, pdb_df)
         self.mk_bonds(lmp)
         self.mk_angles(lmp)
         self.mk_dihedrals(lmp)
         self.mk_pairs(lmp)
 
     def mk_atoms(self,
-                 lmp: relmp.ReadData  # LAMMPS data file
+                 lmp: relmp.ReadData,  # LAMMPS data file
+                 pdb_df: pd.DataFrame  # Final df for pdb file
                  ) -> pd.DataFrame:
-        pass
+        df: pd.DataFrame  # df in the itp format
+        columns: list[str]  # Columns of the DataFrame
+        columns = ['atomnr',  # Index of the atoms in lmp file
+                   'atomtype',  # FF type of the atom, e.g. for c, opls_135
+                   'resnr',  # Index of the residue which atom is belonged
+                   'resname',  # Name of the residue which atom is belonged
+                   'atomname',  # Name of the atom as in PDB file
+                   'chargegrp',  # Charge group as in PDB file
+                   'charge',  # Charge as in the lmp file
+                   'mass'  # Mass odf the atom as in the lmp file
+                   ]
+        df = pd.DataFrame(columns=columns)
 
     def mk_bonds(self,
                  lmp: relmp.ReadData  # LAMMPS data file
                  ) -> pd.DataFrame:
-        pass
+        df: pd.DataFrame  # df in the itp format
+        columns: list[str]  # Columns of the DataFrame
 
     def mk_angles(self,
                   lmp: relmp.ReadData  # LAMMPS data file
                   ) -> pd.DataFrame:
-        pass
+        df: pd.DataFrame  # df in the itp format
+        columns: list[str]  # Columns of the DataFrame
 
     def mk_dihedrals(self,
                      lmp: relmp.ReadData  # LAMMPS data file
                      ) -> pd.DataFrame:
-        pass
+        df: pd.DataFrame  # df in the itp format
+        columns: list[str]  # Columns of the DataFrame
 
     def mk_pairs(self,
                  lmp: relmp.ReadData  # LAMMPS data file
                  ) -> pd.DataFrame:
-        pass
+        df: pd.DataFrame  # df in the itp format
+        columns: list[str]  # Columns of the DataFrame
