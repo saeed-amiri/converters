88576f30a2dac71a1a15f12dbbea9f32bb2b0d9b
Saeed Amiri <samiri@uni-bremen.de>
2023-02-27 11:36:58 +0100

    write down all the angles

--- codes/lmp_itp_pdb.py
+++ codes/lmp_itp_pdb.py
@@ -229,23 +229,28 @@ class WriteItp:
                      ) -> None:
         """write section of the itp file"""
         df_raw: pd.DataFrame  # Copy of the df with mol selected info
-        df_raw = angles[angles['resname'] == mol]
+        df_raw = angles.copy()
         resides_ids = set(df_raw['resnr'])
+        df_list: list[pd.DataFrame] = []  # Keeping all the angles
         if resides_ids:
-            df1: pd.DataFrame  # Copy of the df with mol_id selected info
-            df1 = df_raw[df_raw['resnr'] == list(resides_ids)[0]]
-            ai = [self.__atoms_one[item] for item in df1['ai']]
-            aj = [self.__atoms_one[item] for item in df1['aj']]
-            ak = [self.__atoms_one[item] for item in df1['ak']]
+            for id in resides_ids:
+                df1: pd.DataFrame  # Copy of the df with mol_id selected info
+                df1 = df_raw[df_raw['resnr'] == id]
+                ai = [self.__atoms_one[item] for item in df1['ai']]
+                aj = [self.__atoms_one[item] for item in df1['aj']]
+                ak = [self.__atoms_one[item] for item in df1['ak']]
 
-            df = pd.DataFrame({'ai': ai,
-                               'aj': aj,
-                               'ak': ak,
-                               'funct': df1['funct'],
-                               'theta': df1['theta'],
-                               'cth': df1['cth'],
-                               ' ': df1[' '],
-                               'angle_name': df1['angle_name']})
+                df_i = pd.DataFrame({'ai': ai,
+                                     'aj': aj,
+                                     'ak': ak,
+                                     'funct': df1['funct'],
+                                     'theta': df1['theta'],
+                                     'cth': df1['cth'],
+                                     ' ': df1[' '],
+                                     'angle_name': df1['angle_name']})
+                df_list.append(df_i)
+                del df_i
+            df: pd.DataFrame = pd.concat(df_list)
             header: list[str] = [item for item in df.columns]
             f.write(f'[ angles ]\n')
             f.write(f'; {" ".join(header)}\n')
